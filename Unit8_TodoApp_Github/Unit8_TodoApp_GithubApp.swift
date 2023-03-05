0ae944631f40d72b84490b4638c7461b2ee3bc33
Moez Rehman <moez.rehman.gcu@gmail.com>
2023-03-05 12:00:27 +0400

    Initial Commit

---
+++ Unit8_TodoApp_Github/Unit8_TodoApp_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit8_TodoApp_GithubApp.swift
+//  Unit8_TodoApp_Github
+//
+//  Created by Moez Rehman on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Unit8_TodoApp_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
