c088eead45061febd1c17ecdffbde467b3114af3
Moez Rehman <moez.rehman.gcu@gmail.com>
2023-03-07 10:59:57 +0400

    todos tab added; Observed object

---
+++ Unit8_TodoApp_Github/View/HowManyTodosTab/HowManyMoreView.swift
@@ -0,0 +1,20 @@
+//
+//  HowManyMoreView.swift
+//  Unit8_TodoApp_Github
+//
+//  Created by Moez Rehman on 3/7/23.
+//
+
+import SwiftUI
+
+struct HowManyMoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HowManyMoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        HowManyMoreView()
+    }
+}
